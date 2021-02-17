0e4c28c6844d649856ec9200ce3409e169363102
Sebastian Jäger <message@sebastian-jaeger.me>
2021-02-17 14:34:16 +0100

    refactor review classes to fit new base classes

--- src/jenga/tasks/reviews.py
+++ src/jenga/tasks/reviews.py
@@ -1,3 +1,5 @@
+from typing import Any, Dict, Optional, Tuple
+
 import numpy as np
 import pandas as pd
 from sklearn.compose import ColumnTransformer
@@ -5,9 +7,9 @@ from sklearn.feature_extraction.text import HashingVectorizer
 from sklearn.linear_model import SGDClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OneHotEncoder, StandardScaler, label_binarize
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
-from jenga.basis import BinaryClassificationTask
+from ..basis import BinaryClassificationTask
 
 
 # Predict whether a video game review is considered helpful or not
@@ -35,6 +37,9 @@ class VideogameReviewsTask(BinaryClassificationTask):
             'review_body': str,
             'review_date': str
         }
+        categorical_columns = ['marketplace', 'customer_id', 'review_id', 'product_id', 'product_parent', 'vine', 'verified_purchase']
+        numerical_columns = ['star_rating']
+        text_columns = ['product_title', 'review_headline', 'review_body', 'title_and_review_text']
 
         raw_data = pd.read_csv('../data/reviews/videogames.tsv', sep='\t', names=columns, dtype=dtypes)
         # We drop this column, the prediction task is to easy otherwise
@@ -43,49 +48,50 @@ class VideogameReviewsTask(BinaryClassificationTask):
         raw_data[['product_title', 'review_headline', 'review_body']] = raw_data[['product_title', 'review_headline', 'review_body']].fillna(value='')
         raw_data['title_and_review_text'] = raw_data.product_title + ' ' + raw_data.review_headline + ' ' + raw_data.review_body
 
-        train_data = self.__extract_data(raw_data, '2015-05-04', '2015-06-14')
-        test_data = self.__extract_data(raw_data, '2015-06-15', '2015-06-28')
-        train_labels = self.__extract_labels(raw_data, '2015-05-04', '2015-06-14')
-        test_labels = self.__extract_labels(raw_data, '2015-06-15', '2015-06-28')
+        train_data = self._extract_data(raw_data, '2015-05-04', '2015-06-14')
+        train_labels = self._extract_labels(raw_data, '2015-05-04', '2015-06-14')
+        train_labels = train_labels.astype("category")
+
+        test_data = self._extract_data(raw_data, '2015-06-15', '2015-06-28')
+        test_labels = self._extract_labels(raw_data, '2015-06-15', '2015-06-28')
+        test_labels = test_labels.astype("category")
 
         super().__init__(
-            seed,
-            train_data,
-            train_labels,
-            test_data,
-            test_labels,
-            categorical_columns=[
-                'marketplace',
-                'customer_id',
-                'review_id',
-                'product_id',
-                'product_parent',
-                'vine',
-                'verified_purchase'
-            ],
-            numerical_columns=['star_rating'],
-            text_columns=[
-                'product_title',
-                'review_headline',
-                'review_body',
-                'title_and_review_text'
-            ]
+            train_data=train_data,
+            train_labels=train_labels,
+            test_data=test_data,
+            test_labels=test_labels,
+            categorical_columns=categorical_columns,
+            numerical_columns=numerical_columns,
+            text_columns=text_columns,
+            is_image_data=False,
+            seed=seed
         )
 
     @staticmethod
-    def __extract_data(raw_data, start_date, end_date):
+    def _extract_data(raw_data, start_date, end_date):
         data_slice = raw_data[(raw_data.review_date >= start_date) & (raw_data.review_date <= end_date)].copy(deep=True)
         data_slice = data_slice.drop(['helpful_votes'], axis=1)
         return data_slice
 
     @staticmethod
-    def __extract_labels(raw_data, start_date, end_date):
+    def _extract_labels(raw_data, start_date, end_date):
         data_slice = raw_data[(raw_data.review_date >= start_date) & (raw_data.review_date <= end_date)].copy(deep=True)
-        labels = np.ravel(label_binarize(data_slice.helpful_votes > 0, [True, False]))
+        return (data_slice['helpful_votes'] > 0).replace({True: 1, False: 0})
 
-        return labels
+    def _get_pipeline_grid_scorer_tuple(self, feature_transformation: ColumnTransformer) -> Tuple[Dict[str, object], Any, Dict[str, Any]]:
+        pass
 
-    def fit_baseline_model(self, train_data, train_labels):
+    def fit_baseline_model(self, train_data: Optional[pd.DataFrame] = None, train_labels: Optional[pd.Series] = None):
+
+        if (train_data is None and train_labels is not None) or (train_data is not None and train_labels is None):
+            raise ValueError("either set both parameters (train_data, train_labels) or non")
+
+        use_original_data = train_data is None
+
+        if use_original_data:
+            train_data = self.train_data
+            train_labels = self.train_labels
 
         numerical_attributes = ['star_rating']
         categorical_attributes = ['vine', 'verified_purchase']
@@ -113,4 +119,10 @@ class VideogameReviewsTask(BinaryClassificationTask):
         )
 
         search = GridSearchCV(pipeline, param_grid, scoring='roc_auc', cv=5, n_jobs=-1)
-        return search.fit(train_data, train_labels)
+        model = search.fit(train_data, train_labels).best_estimator_
+
+        # only set baseline model attribute if it is trained on the original task data
+        if use_original_data:
+            self._baseline_model = model
+
+        return model
