08f35b9e7559d63927281471f599569f77997a08
Sebastian Jäger <message@sebastian-jaeger.me>
2021-02-17 14:36:01 +0100

    refactor  corruption impact evaluator to fit new base classes

--- src/jenga/evaluation/corruption_impact.py
+++ src/jenga/evaluation/corruption_impact.py
@@ -1,28 +1,22 @@
-class ValidationResult:
+from sklearn.base import BaseEstimator
 
-    def __init__(self, corruption, baseline_score, corrupted_scores):
-        self.corruption = corruption
-        self.baseline_score = baseline_score
-        self.corrupted_scores = corrupted_scores
+from .basis import DataCorruption, Evaluator, ValidationResult
 
 
 # Evaluate the impact of one or more data corruption on the prediction quality of a model;
 # applies the corruptions repeatedly to copies of the test data
 # and computes the model's prediction quality for afterwards
-class CorruptionImpactEvaluator:
+class CorruptionImpactEvaluator(Evaluator):
 
-    def __init__(self, task):
-        self.__task = task
+    def evaluate(self, model: BaseEstimator, num_repetitions: int, *corruptions: DataCorruption):
 
-    def evaluate(self, model, num_repetitions, *corruptions):
-
-        if not self.__task.is_image_data:
-            test_data_copy = self.__task.test_data.copy(deep=True)
+        if not self._task.is_image_data:
+            test_data_copy = self._task.test_data.copy(deep=True)
         else:
-            test_data_copy = self.__task.test_data.copy()
+            test_data_copy = self._task.test_data.copy()
 
-        baseline_predictions = model.predict_proba(test_data_copy)
-        baseline_score = self.__task.score_on_test_data(baseline_predictions)
+        baseline_predictions = self._model_predict(model, test_data_copy)
+        baseline_score = self._task.score_on_test_data(baseline_predictions)
 
         results = []
 
@@ -36,17 +30,18 @@ class CorruptionImpactEvaluator:
         for corruption in corruptions:
             corrupted_scores = []
             # Repeatedly
-            for _ in range(0, num_repetitions):
+            for _ in range(num_repetitions):
 
-                if not self.__task.is_image_data:
-                    test_data_copy = self.__task.test_data.copy(deep=True)
+                if not self._task.is_image_data:
+                    test_data_copy = self._task.test_data.copy(deep=True)
                 else:
-                    test_data_copy = self.__task.test_data.copy()
+                    test_data_copy = self._task.test_data.copy()
 
                 corrupted_data = corruption.transform(test_data_copy)
 
-                corrupted_predictions = model.predict_proba(corrupted_data)
-                corrupted_score = self.__task.score_on_test_data(corrupted_predictions)
+                # corrupted_predictions = model.predict_proba(corrupted_data)
+                corrupted_predictions = self._model_predict(model, corrupted_data)
+                corrupted_score = self._task.score_on_test_data(corrupted_predictions)
 
                 corrupted_scores.append(corrupted_score)
 
