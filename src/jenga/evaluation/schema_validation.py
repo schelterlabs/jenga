4938400587e2f9aff9b50d41111e81972ba42d8d
Sebastian Jäger <message@sebastian-jaeger.me>
2021-02-17 14:36:47 +0100

    refactor schema evaluator class to fit new base classes

--- src/jenga/evaluation/schema_validation.py
+++ src/jenga/evaluation/schema_validation.py
@@ -1,33 +1,39 @@
 import tensorflow_data_validation as tfdv
+from sklearn.base import BaseEstimator
+
+from ..basis import DataCorruption
+from .basis import Evaluator, ValidationResult
 
 
-class SchemaValidationResult:
+class SchemaValidationResult(ValidationResult):
 
     def __init__(self, corruption, anomalies, baseline_score, corrupted_scores):
-        self.corruption = corruption
+
+        super().__init__(
+            corruption=corruption,
+            baseline_score=baseline_score,
+            corrupted_scores=corrupted_scores
+        )
         self.anomalies_detected = [len(exp_anomalies) > 0 for exp_anomalies in anomalies]
-        self.baseline_score = baseline_score
-        self.corrupted_scores = corrupted_scores
 
 
 # Takes a tfdv schema and corruptions to evaluate two things at once:
 # (1) Does tfdv detect an anomaly for the corruption?
 # (2) How much does the corruption decrease the prediction quality
-class SchemaValidationEvaluator:
-
-    def __init__(self, task):
-        self.__task = task
+class SchemaValidationEvaluator(Evaluator):
 
     # Auto-infer a schema from the training daya
     def schema_from_train_data(self):
-        train_data_stats = tfdv.generate_statistics_from_dataframe(self.__task.train_data)
+        train_data_stats = tfdv.generate_statistics_from_dataframe(self._task.train_data)
         schema = tfdv.infer_schema(statistics=train_data_stats)
         return schema
 
-    def evaluate_validation(self, model, schema, num_repetitions, *corruptions):
+    def evaluate(self, model: BaseEstimator, num_repetitions: int, *corruptions: DataCorruption):
 
-        baseline_predictions = model.predict_proba(self.__task.test_data)
-        baseline_score = self.__task.score_on_test_data(baseline_predictions)
+        schema = self.schema_from_train_data()
+
+        baseline_predictions = model.predict_proba(self._task.test_data)
+        baseline_score = self._task.score_on_test_data(baseline_predictions)
 
         results = []
 
@@ -36,7 +42,7 @@ class SchemaValidationEvaluator:
             corrupted_scores = []
             anomalies = []
             for _ in range(0, num_repetitions):
-                test_data_copy = self.__task.test_data.copy(deep=True)
+                test_data_copy = self._task.test_data.copy(deep=True)
                 corrupted_data = corruption.transform(test_data_copy)
 
                 # Determine whether tfdv finds anomalies in the data
@@ -47,7 +53,7 @@ class SchemaValidationEvaluator:
 
                 # Compute the prediction score on the test data
                 corrupted_predictions = model.predict_proba(corrupted_data)
-                corrupted_score = self.__task.score_on_test_data(corrupted_predictions)
+                corrupted_score = self._task.score_on_test_data(corrupted_predictions)
 
                 anomalies.append(schema_anomalies)
                 corrupted_scores.append(corrupted_score)
